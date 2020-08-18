f767d2036455786f77312ce2c173b738f10572c6
Hemanth Krishna <hkpdev008@gmail.com>
2020-08-18 08:35:18 +0530

    init: Basic GoBot

---
+++ src/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("We Good ")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error Loading env File")
+	}
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Fatalf("Error Initialising bot due to %s", err.Error())
+	}
+	bot.Debug = true
+	log.Printf("%s Bot is up and running", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		switch update.Message.Command() {
+		case "sayhi":
+			msg.Text = "Hi ! :)"
+		default:
+			msg.Text = "I do not know that command !"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
+}
